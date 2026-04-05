86f87b3b35478e62adb39081c1eec929110cf72a
Eng.Mostafa Sedhom <elmohandes24680@gmail.com>
2026-04-06 00:01:53 +0200

    handle text folder and keep two functions onlt text and text overflow

--- SEDHOM_Display_OS/src/SEDHOM_Draw_GUI/SEDHOM_Draw_Text/SEDHOM_Draw_Text.cpp
+++ SEDHOM_Display_OS/src/SEDHOM_Draw_GUI/SEDHOM_Draw_Text/SEDHOM_Draw_Text.cpp
@@ -1,6 +1,7 @@
 //TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
 #include "SEDHOM_Draw_Text.h"
 //TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
+// private functions definitions
 void SEDHOM_Draw_Text::Char(Coordinate_t coordinate,const SEDHOM_GFXfont* font,Color_t color,char c)
 {
     Draw_Char(coordinate.x, coordinate.y, font, color, c);
@@ -9,23 +10,26 @@ void SEDHOM_Draw_Text::Text(Coordinate_t coordinate,const SEDHOM_GFXfont* font,C
 {
     Draw_String(coordinate.x, coordinate.y, font, color, txt);
 }
+// public functions definitions
+
+const SEDHOM_Text_Style SEDHOM_Draw_Text::Default_Style = SEDHOM_Text_Style().font(Font()).color(Color_Blue);
 void SEDHOM_Draw_Text::Text(Coordinate_t coordinate,const char* txt,SEDHOM_Text_Style style)
 {
-    // Draw_String(coordinate.x, coordinate.y, style.getFont().return_font(), style.getColor(), txt);
     Text(coordinate,style.getFont().return_font(),style.getColor(),txt);
 }
-void SEDHOM_Draw_Text::Text(Coordinate_t coordinate,const SEDHOM_GFXfont* font,Color_t color,Dynamic val,int float_precision)
+void SEDHOM_Draw_Text::Text(Coordinate_t coordinate,Dynamic val,int float_precision,SEDHOM_Text_Style style)
 {
-  Text(coordinate, font, color, val.to_String(float_precision));
+    Text(coordinate,style.getFont().return_font(),style.getColor(),val.to_String(float_precision));
 }
-void SEDHOM_Draw_Text::Text_OverFlow(Coordinate_t coordinate,const SEDHOM_GFXfont* font, Color_t color,char * txt,int number_overFlow,char* overFlow_chars)
+void SEDHOM_Draw_Text::Text_OverFlow(Coordinate_t coordinate,char* txt,SEDHOM_Text_Style style,int number_overFlow,char* overFlow_chars)
 {
+    // number_overFlow -= strlen(overFlow_chars);
     char buffer[100]; 
     int txt_len = strlen(txt);
     int overflow_len = strlen(overFlow_chars);
     if(txt_len <= number_overFlow)
     {
-        Text(coordinate, font, color, txt);
+        Text(coordinate,txt,style);
         return;
     }
     int copy_len = number_overFlow - overflow_len;
@@ -33,6 +37,6 @@ void SEDHOM_Draw_Text::Text_OverFlow(Coordinate_t coordinate,const SEDHOM_GFXfon
     strncpy(buffer, txt, copy_len);
     buffer[copy_len] = '\0';
     strcat(buffer, overFlow_chars);
-    Text(coordinate, font, color, buffer);
+    Text(coordinate,buffer,style);
 }
 //TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
--- SEDHOM_Display_OS/src/SEDHOM_Draw_GUI/SEDHOM_Draw_Text/SEDHOM_Draw_Text.h
+++ SEDHOM_Display_OS/src/SEDHOM_Draw_GUI/SEDHOM_Draw_Text/SEDHOM_Draw_Text.h
@@ -10,13 +10,13 @@
 class SEDHOM_Draw_Text : private SEDHOM_GUI_Core , private SEDHOM_Font_definations
 {
     private:
-        /* data */
-    public:
         void Char(Coordinate_t coordinate,const SEDHOM_GFXfont* font,Color_t color,char c);
         void Text(Coordinate_t coordinate,const SEDHOM_GFXfont* font,Color_t color,char* txt);
-        void Text(Coordinate_t coordinate,const char* txt,SEDHOM_Text_Style style = SEDHOM_Text_Style().font(Font()).color(Color_Blue));
-        void Text(Coordinate_t coordinate,const SEDHOM_GFXfont* font,Color_t color,Dynamic val,int float_precision = 2);
-        void Text_OverFlow(Coordinate_t coordinate,const SEDHOM_GFXfont* font, Color_t color,char * txt,int number_overFlow = 10,char* overFlow_chars = "...");
+    public:
+        static const SEDHOM_Text_Style Default_Style ;
+        void Text(Coordinate_t coordinate,const char* txt,SEDHOM_Text_Style style = Default_Style);
+        void Text(Coordinate_t coordinate,Dynamic val,int float_precision = 2,SEDHOM_Text_Style style = Default_Style);
+        void Text_OverFlow(Coordinate_t coordinate,char* txt,SEDHOM_Text_Style style = Default_Style,int number_overFlow = 10,char* overFlow_chars = "...");
 
 };
 //TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
